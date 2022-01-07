3f6f38385a54d2cbe9dd3225be811a0341de8938
shk3029 <shk30290@naver.com>
2022-01-07 22:32:37 +0900

    Initial commit

---
+++ src/main/java/com/example/studyeureka/StudyEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/studyeureka/StudyEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studyeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
